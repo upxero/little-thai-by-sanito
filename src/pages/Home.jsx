7dd6276ec165d214e120217761a725e105f27bf3
Michael Bernard <mike_bernard@live.nl>
2025-12-30 03:43:55 +0100

    add pic to gallery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,7 +42,7 @@ export default function Home() {
         </script>
       </Helmet>
       
-      <TopBarAnnouncement />
+      {/* <TopBarAnnouncement /> */}
 
       <HeroSiderTwo />
       <MovingText />
